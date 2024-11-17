cdad92dbda7ebaf91b875680a819eabfa4522866
LorenzoBalderrama <zoupdevops@gmail.com>
2024-11-16 19:42:28 -0800

    Firebase - uploaded site to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,8 @@ function App() {
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-black flex">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 w-full">
         <h1 className="text-5xl font-bold text-center text-red-500 mb-8 font-times">
-          Marvel Universe Search
+          Comic Universe Search
         </h1>
-        
         {!showSearch ? (
           <div className="text-center">
             <p className="text-white text-xl mb-8">
